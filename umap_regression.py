7a75c02b79a17aa998cd03979dc2b032f259b5b2
Jieling-Cai <93742984+Jieling-Cai@users.noreply.github.com>
2022-04-27 01:49:47 -0400

    Update umap_regression.py

--- umap_regression.py
+++ umap_regression.py
@@ -40,12 +40,13 @@ def normalization(train_data_path, test_data):
     return test_data
 
 if __name__ == '__main__':
-    train_data_path = '/output/train_data.npy'
-    test_data_path = '/output/test_data.npy'
-    test_labels_path = '/output/test_chi_square.npy'
-    model_path = '/NN/model.h5'
-    save_fig_path = '/NN/umap.png'
-
+    argparser = argparse.ArgumentParser(sys.argv[0])
+    argparser.add_argument("--train_data_path", type=str, default='/output/train_data.npy')
+    argparser.add_argument("--test_data_path", type=str, default='/output/test_data.npy')
+    argparser.add_argument("--test_labels_path", type=str, default='/output/test_chi_square.npy')
+    argparser.add_argument("--model_path", type=str,  default='/NN/umap.png')
+    args = argparser.parse_args()
+    
     test_data, test_labels = get_data_label_list(test_data_path, test_labels_path)
     sort_index = sorted(range(len(test_labels)), key=lambda k: test_labels[k], reverse=True)
     test_data = test_data[sort_index,:]
@@ -57,25 +58,17 @@ if __name__ == '__main__':
 
     sig_index = np.random.choice(np.arange(40000), 5000, replace=False)
     insig_index = np.random.choice(np.arange(len_test)[-40000:], 5000, replace=False)
-    # insig_index = np.random.choice(np.arange(len_test-int(len_test*0.01))+int(len_test*0.01), 2000, replace=False)
 
-    # test_data = np.concatenate((test_data[sig_index,:],test_data[insig_index,:]),axis=0)
-    # test_labels = np.concatenate((test_labels[sig_index],test_labels[insig_index]),axis=None)
-    # test_words = np.concatenate((test_words[sig_index],test_words[insig_index]),axis=None)
     sig_data = test_data[sig_index,:]
     insig_data = test_data[insig_index,:]
     sig_labels = test_labels[sig_index]
     insig_labels = test_labels[insig_index]
 
-    # data = np.concatenate((sig_data,insig_data),axis=0)
-    # labels = np.concatenate((sig_labels,insig_labels),axis=None)
-
     from tensorflow.keras.models import load_model
     from tensorflow.keras.models import Model
     model = load_model(model_path)
     layer_model = Model(inputs=model.input, outputs=model.get_layer('dense_5').output)
-    # feats = np.asarray(layer_model.predict(data))
-    # embedd = umap.UMAP(n_neighbors=3).fit_transform(feats)
+
     print(sig_data.shape)
     print(insig_data.shape)
 
@@ -85,12 +78,9 @@ if __name__ == '__main__':
     sig_embedd = umap.UMAP(n_neighbors=100).fit_transform(sig_feats)
     insig_embedd = umap.UMAP(n_neighbors=100).fit_transform(insig_feats)
 
-    # plt.scatter(sig_embedd[:, 0], sig_embedd[:, 1], s=10, c=test_labels, cmap='viridis')
-    # plt.colorbar()
     plt.scatter(sig_embedd[:, 0], sig_embedd[:, 1], s=10, alpha=0.8, color='indigo', label='Significant SNPs')
     plt.scatter(insig_embedd[:, 0], insig_embedd[:, 1], s=10, color='darkgreen',  alpha=0.8, label='Insignificant SNPs')
-    # plt.scatter(embedd[:len(sig_index), 0], embedd[:len(sig_index), 1], s=10, color='indigo', label='Frequent Words')
-    # plt.scatter(embedd[-len(insig_data):, 0], embedd[-len(insig_data):, 1], s=10, color='darkgreen', label='Infrequent Words')
+
     plt.legend(loc='upper right')
     plt.savefig(save_fig_path)
     plt.show()
